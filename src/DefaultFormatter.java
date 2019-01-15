89904295cde801767004d92024023fc462e28da6
Philip Wellener <42978547+philipwellener@users.noreply.github.com>
2019-01-14 22:26:00 -0500

    Add files via upload

---
+++ src/DefaultFormatter.java
@@ -0,0 +1,18 @@
+
+public class DefaultFormatter implements NumberFormatter
+{
+	public String format(int num)
+	{
+		String str = "" + num;
+		String form = "";
+		for(int i = 0; i<str.length(); i++)
+		{
+			 form += str.charAt(str.length()-i);
+			 if(i % 3 == 0  && i != str.length())
+			 {
+				 form += ",";
+			 }
+		}
+		return form;
+	}
+}
